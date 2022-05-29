c80325445405eb19b66b3b75fc86ef3e766143b8
ntsubasao <71168443+ntsubasao@users.noreply.github.com>
2022-05-29 15:31:00 +0900

    useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColofulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0); //簡単に動的な値を作ることができる(第一引数のところにはnumなど任意の変数名を入れられる(動的に変わるnum)、第2引数numを変更するための関数(numを更新するためのsetnum)）
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    console.log("useEffect!!");
+  }, [num]); //一回だけ表示させて再レンダリングしたくない場合は、第2引数の[]配列を空にする(よく使うらしい),値を入れた場合はその値にだけ関心をもつ(今回ならnum)
+  if (num > 0) {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true); //左がFalseの場合、右側の処理をやってくれる(論理演算子を使わないと再レンダリングでエラーがおきる)
+    } else {
+      faceShowFlag && setFaceShowFlag(false); //左がTrueの場合、右側を返す
+    }
+  }
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColofulMessage color="blue">お元気ですか</ColofulMessage>
       <ColofulMessage color="pink">元気です！！</ColofulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(*´ω｀*)</p>}
     </>
   );
 }; //propsとして、colorを今回は入れる（名前は任意）
+//&&(かつ)左の要素がTrueのとき、左を返すという機能がある(FaceShowFlagがTrueのとき,絵文字をレンダリングする)
 
 export default App; //他のファイルでも使えるようにしている
