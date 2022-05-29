a4f2a2c1e57ea250005fa5a55b61118aa5a6a61b
ntsubasao <71168443+ntsubasao@users.noreply.github.com>
2022-05-29 11:35:04 +0900

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColofulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0); //簡単に動的な値を作ることができる(第一引数のところにはnumなど任意の変数名を入れられる(動的に変わるnum)、第2引数numを変更するための関数(numを更新するためのsetnum)）
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColofulMessage color="blue">お元気ですか</ColofulMessage>
       <ColofulMessage color="pink">元気です！！</ColofulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 }; //propsとして、colorを今回は入れる（名前は任意）
